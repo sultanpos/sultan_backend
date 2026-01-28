6d6ce33eb9aeb8cbe8e13be6c50dbce2ea0f4cee
apin <apin.klas@gmail.com>
2026-01-28 19:59:33 +0700

    refactor: improve formatting of UnitOfMeasureRepository trait methods

--- sultan_core/src/storage/unit.rs
+++ sultan_core/src/storage/unit.rs
@@ -8,14 +8,36 @@ use crate::domain::{
 
 #[async_trait]
 pub trait UnitOfMeasureRepository<DB>: Send + Sync {
-    async fn create<'e, E>(&self, ctx: &Context, e: E,   id: i64, uom: &UnitOfMeasureCreate) -> DomainResult<()>
-        where E: Executor<'e, Database = DB>;
-    async fn update<'e, E>(&self, ctx: &Context, e: E,id: i64, uom: &UnitOfMeasureUpdate) -> DomainResult<()>
-    where E: Executor<'e, Database = DB>;
+    async fn create<'e, E>(
+        &self,
+        ctx: &Context,
+        e: E,
+        id: i64,
+        uom: &UnitOfMeasureCreate,
+    ) -> DomainResult<()>
+    where
+        E: Executor<'e, Database = DB>;
+    async fn update<'e, E>(
+        &self,
+        ctx: &Context,
+        e: E,
+        id: i64,
+        uom: &UnitOfMeasureUpdate,
+    ) -> DomainResult<()>
+    where
+        E: Executor<'e, Database = DB>;
     async fn delete<'e, E>(&self, ctx: &Context, e: E, id: i64) -> DomainResult<()>
-    where E: Executor<'e, Database = DB>;
+    where
+        E: Executor<'e, Database = DB>;
     async fn get_all<'e, E>(&self, ctx: &Context, e: E) -> DomainResult<Vec<UnitOfMeasure>>
-    where E: Executor<'e, Database = DB>;
-    async fn get_by_id<'e, E>(&self, ctx: &Context, e: E, id: i64) -> DomainResult<Option<UnitOfMeasure>>
-    where E: Executor<'e, Database = DB>;
+    where
+        E: Executor<'e, Database = DB>;
+    async fn get_by_id<'e, E>(
+        &self,
+        ctx: &Context,
+        e: E,
+        id: i64,
+    ) -> DomainResult<Option<UnitOfMeasure>>
+    where
+        E: Executor<'e, Database = DB>;
 }
